dfe1384124504e4b21a332ec5f592cf88a71c3c9
merry <merrykang1103@gmail.com>
2023-10-13 18:02:19 +0900

    [Practice] intro_nomad

---
+++ basic_nomad/app.js
@@ -0,0 +1,15 @@
+import "./app.css";
+
+function app() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <p>
+          Edit <code>src</code>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default app;
